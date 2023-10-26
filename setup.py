3ba2d4e03cae75a2d9f5f7ecf2abc9d8b34ba6ed
Gabriel Won-Held <gabrielwonheld@github.com>
2023-10-25 21:11:38 -0300

    refatorando com codigo próprio

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name='pingnet',
-    packages=find_packages(),
-    entry_points={
-          'console_scripts': ['pingnet=src.pingnet:main',],
-    },
-)
