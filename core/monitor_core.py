efced0d6e7a1906189072f3f1937ae37435490cb
gabrielwonheld <gabrielwonheld@github.com>
2024-07-23 18:00:23 -0300

    Refatoração

--- core/monitor_core.py
+++ core/monitor_core.py
@@ -3,14 +3,15 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from view.display import MonitDisplay
 from utils.clear_utils import Clean
 import threading
+from view.banner import Banner
 
 lock = threading.Lock()
 
 class Monit:
 
     def __init__(self, HOST, PORT):
-        #self.hosts = config['hosts']
-        #self.hosts = config['hosts']
+        
+
         self.host = HOST
         self.port = PORT
         
@@ -40,12 +41,10 @@ class Monit:
         return results
 
 
-    def all_process_results(self):
+    def separete_results(self):
         
         results = self.all_run_threads(check_host)
-        self.list_total_host.clear()
-        self.list_active_host.clear()
-        self.list_inactive_host.clear()
+
 
         for nome, status in results:
             self.list_total_host.append(nome)
@@ -53,44 +52,70 @@ class Monit:
                 self.list_active_host.append(nome)
             else:
                 self.list_inactive_host.append(nome)
-
-        self.list_total_host.sort()
-        self.list_active_host.sort()
-        self.list_inactive_host.sort()
-        
-            
-    '''def difList(self,previous_list, current_list):
-            if previous_list != current_list:
                 
-                return self.list_active_host.copy()
-            return previous_list'''
+       
+        
+
 
     def get_active(self):
 
             with lock:
                 
-                self.all_process_results()
+                Clean.clear_list(self.list_active_host,self.list_inactive_host,self.list_total_host)
+                
+                self.separete_results()
+
+                self.list_total_host.sort()
+                self.list_active_host.sort()
+                self.list_inactive_host.sort()
+                
                 if self.list_previous_active_host != self.list_active_host:
+
                     Clean.clear_terminal()
+                    Banner.sharingan()
+
                     display_host = MonitDisplay(active=self.list_active_host, total=self.list_total_host)
-                    display_host.dysplay_active()
+                    display_host.display_active()
+                    display_host.display_total()
                     self.list_previous_active_host = self.list_active_host.copy()
+                    
                      
                 
             return self.list_previous_active_host
 
 
 
-
-
-
-
-    def get_inactive(self):
-       #inactive = self.all_run_threads(check_host)
-       self.all_process_results()
-       display_host = MonitDisplay(inactive=self.list_inactive_host,total=self.list_total_host)
-       return display_host.dysplay_inactive()
        
+    def get_inactive(self):
+
+            with lock:
+                
+                self.separete_results()
+                if self.list_previous_inactive_host != self.list_inactive_host:
+
+                    Clean.clear_terminal()
+                    Banner.sharingan()
+                    display_host = MonitDisplay(inactive=self.list_inactive_host, total=self.list_total_host)
+                    #display_host.display_inactive()
+                    display_host.display_total()
+                    self.list_previous_inactive_host = self.list_inactive_host.copy()  
+
+            return self.list_previous_inactive_host
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
