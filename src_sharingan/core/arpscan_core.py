d40fdb2580a6b9fa91afb60c35bc85d73dac6253
gabrielwonheld <gabrielwonheld@github.com>
2025-04-07 19:29:03 -0300

    Retornando ip e mac por tuplas

--- src_sharingan/core/arpscan_core.py
+++ src_sharingan/core/arpscan_core.py
@@ -14,19 +14,15 @@ class ArpScan:
 				timeout = 2, 
 				iface = interface,
 				inter = 0.1)
-		print(ans)
+		#print(ans)
 		print ("\n[*] IP - MAC") 
 		for snd,rcv in ans: 
+			ip = rcv[ARP].psrc
+			mac = rcv[Ether].src
 			#print(rcv.sprintf(r"%ARP.psrc% - %Ether.src%"))
-			list_arp.append(rcv.sprintf(r"%ARP.psrc% - %Ether.src%"))
-			print(rcv.split())
+			list_arp.append((ip,mac))
+			#print(rcv)
 		return list_arp
-		"""stop_time = datetime.now()
-		total_time = stop_time - start_time 
-		print("\n[*] Scan Complete. Duration:", total_time)
-
-	if __name__ == "__main__":
-		arp_scan(sys.argv[1], sys.argv[2])"""
 
 scan = ArpScan.arp_scan(sys.argv[1], sys.argv[2])
 
